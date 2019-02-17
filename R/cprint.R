efeaf0f40386f184211ad47aaa8901d07f62b892
b780620 <Brandon.Greenwell@8451.com>
2019-02-17 10:52:34 -0500

    tweaks to pass R CMD checks

--- R/cprint.R
+++ R/cprint.R
@@ -1,7 +1,7 @@
-#' Copy Print
+#' Copy print
 #'
-#' \code{cprint()}  prints common R data structures in a format which can be
-#' used as input to the interpreter.
+#' The \code{cprint} function provides a capability to print common R data
+#' structures in a form which can be used as input to the interpreter.
 #'
 #' @param x An appropriate R object.
 #'
@@ -12,8 +12,7 @@
 #' @export
 #'
 #' @examples
-#'
-#' cprint(head(iris))  # output can easily be copied into a script file
+#' cprint(iris)  # output can easily be copied into an RStudio script file
 cprint <- function(x, ...) {
   UseMethod("cprint")
 }
@@ -32,7 +31,16 @@ cprint.numeric <- function(x, ...) {
 #'
 #' @export
 cprint.character <- function(x, ...) {
-  string <- paste0("c(", paste0("'", x, "'", collapse = ", "), ")")
+  string <- paste0("c(", paste0("\"", x, "\"", collapse = ", "), ")")
+  cat(string, "\n")
+}
+
+
+#' @rdname cprint
+#'
+#' @export
+cprint.factor <- function(x, ...) {
+  string <- paste0("factor(c(", paste0("\"", x, "\"", collapse = ", "), "))")
   cat(string, "\n")
 }
 
@@ -45,11 +53,23 @@ cprint.data.frame <- function(x, ...) {
   cat("data.frame(", "\n", ...)
   for (i in seq_len(length(cnames) - 1)) {
     if (is.numeric(x[[i]])) {
-      cat(" ", cnames[i], "=", paste0("c(", paste0(x[[i]], collapse = ", "), "),"), "\n", ...)
+      cat(" ", cnames[i], "=",
+          paste0("c(", paste0(x[[i]],
+                              collapse = ", "), "),"), "\n", ...)
     } else {
-      cat(" ", cnames[i], "=", paste0("c(", paste0("'", x[[i]], "'", collapse = ", "), "),"), "\n", ...)
+      cat(" ", cnames[i], "=",
+          paste0("c(", paste0("\"", x[[i]], "\"",
+                              collapse = ", "), "),"), "\n", ...)
     }
   }
-  cat(" ", cnames[length(cnames)], "=", paste0("c(", paste0("'", x[[length(cnames)]], "'", collapse = ", "), ")"), "\n", ...)
+  if (is.numeric(x[[length(cnames)]])) {
+    cat(" ", cnames[length(cnames)], "=",
+        paste0("c(", paste0(x[[length(cnames)]],
+                            collapse = ", "), ")"), "\n", ...)
+  } else {
+    cat(" ", cnames[length(cnames)], "=",
+        paste0("c(", paste0("\"", x[[length(cnames)]], "\"",
+                            collapse = ", "), ")"), "\n", ...)
+  }
   cat(")", "\n", ...)
 }
--- README.md
+++ README.md
@@ -1,24 +1,49 @@
-cprint: Copy Print
-==================
 
-Coming soon!
+# cprint: Copy Print
 
-Installation
-------------
+This is a simple package to solve an issue I run into quite often:
+convert a simple data structure I am working with (i.e., a vector or
+data frame) into code that can be copy and pasted into an R script or
+the interpreter that will reproduce the original object.
 
-Basic usage
------------
+## Installation
+
+Install from GitHub:
 
 ``` r
-# Output can be copy and pasted directly into a script or the R interpreter
+if (!requireNamespace("devtools")) {
+  install.packages("devtools")
+}
+devtools::install_github("bgreenwell/cprint")
+```
+
+## Basic usage
+
+Output can be copy and pasted directly into a script or the R
+interpreter:
+
+``` r
 library(cprint)
-cprint(head(iris))
+
+cprint(iris[1:5, ])
 ```
 
     ## data.frame( 
-    ##   Sepal.Length = c(5.1, 4.9, 4.7, 4.6, 5, 5.4), 
-    ##   Sepal.Width = c(3.5, 3, 3.2, 3.1, 3.6, 3.9), 
-    ##   Petal.Length = c(1.4, 1.4, 1.3, 1.5, 1.4, 1.7), 
-    ##   Petal.Width = c(0.2, 0.2, 0.2, 0.2, 0.2, 0.4), 
-    ##   Species = c('setosa', 'setosa', 'setosa', 'setosa', 'setosa', 'setosa') 
+    ##   Sepal.Length = c(5.1, 4.9, 4.7, 4.6, 5), 
+    ##   Sepal.Width = c(3.5, 3, 3.2, 3.1, 3.6), 
+    ##   Petal.Length = c(1.4, 1.4, 1.3, 1.5, 1.4), 
+    ##   Petal.Width = c(0.2, 0.2, 0.2, 0.2, 0.2), 
+    ##   Species = c("setosa", "setosa", "setosa", "setosa", "setosa") 
     ## )
+
+``` r
+cprint(iris$Petal.Width[1:5])
+```
+
+    ## c(0.2, 0.2, 0.2, 0.2, 0.2)
+
+``` r
+cprint(iris$Species[1:5])
+```
+
+    ## factor(c("setosa", "setosa", "setosa", "setosa", "setosa"))
